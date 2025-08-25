31733a0bc6eac1069a1ed3250f233dea96bc17c4
Kushagra Jadiya <kjadiya2004@gmail.com>
2025-08-26 02:07:51 +0530

    Clerk Authentication and Authorization Working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignOutButton } from "@clerk/nextjs";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      Home Page
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default HomePage;
